e565b6f130f5b02f6f4a111f8811f22960cdb0a7
qwewerertrty <37083556+qwewerertrty@users.noreply.github.com>
2018-03-05 21:17:30 +0100

    Update JavaMethods.java

--- First/JavaMethods.java
+++ First/JavaMethods.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class CleanFile {
+	static final int MAX_LENGTH = 11;
+	static final int MIN_LENGTH = 8;
 	
 	public void cleanAndPrintAfterSlash(String input_file, String output_file) throws IOException {
 		// file input
@@ -22,13 +24,13 @@ public class CleanFile {
         // Array finale da stampare su file
         String[] finalWords;
         
-        try{
-            while ((line = reader.readLine()) != null){
+        try {
+            while ((line = reader.readLine()) != null) {
                 if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') ||
                         (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
                     words.add(line);
             }
-        }catch(NullPointerException e){ // fine della lista / lista vuota
+        }catch(NullPointerException e) { // fine della lista / lista vuota
             System.out.println("End");
         }
         // inserisco i caratteri nell'array e stampo
@@ -38,6 +40,41 @@ public class CleanFile {
         }
         reader.close();
         writer.close();
+	}
+	
+	// ritarna il numero di parole comprese tra 8 e 11 (di lunghezza)
+	public int wordsBetweenEightAndEleven(String input_file) throws IOException {
+		// file input
+        File fin = new File(input_file);
+        // oggetto reader con codifica dei caratteri UTF-16
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(fin), "UTF-16"));
+        // stringa per leggere ogni riga del file
+        String line;
+        // ArrayList per rimuovere tutte le parole dopo '/'
+        ArrayList<String> words = new ArrayList<>();
+        // Array finale da stampare su file
+        String[] finalWords;
+        
+        try {
+            while ((line = reader.readLine()) != null) {
+                if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') ||
+                        (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
+                    words.add(line);
+            }
+        }catch(NullPointerException e) { // fine della lista / lista vuota
+            System.out.println("End");
+        }
+        int counter = 0;
+        // controllo la lunghezza della parola
+        for (String element : words) {
+            finalWords = element.split("/");
+            if (finalWords[0].length() <= MAX_LENGTH && finalWords[0].length() >= MIN_LENGTH) {
+            	counter++;
+            }
+        }
+        reader.close();
+        return counter;
 	}
 	
 	// genera anagrammi
@@ -78,15 +115,14 @@ public class CleanFile {
 package fiero;
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	
     public static void main(String[] args) throws IOException{
     	CleanFile c = new CleanFile();
     	
-    	String in = "list.txt";   // file input
-    	String out = "monco.txt"; // file output
+    	String in = "it_IT.txt";   // file input
+    	String out = "output.txt"; // file output
     	
     	String m = "cin";
     	
@@ -94,6 +130,7 @@ public class Main {
     
     	System.out.println(c.anagram(m));
     	System.out.println(c.numbersOfAnagrams(c.anagram(m)));
+    	System.out.println(c.wordsBetweenEightAndEleven(in));
     }
 
 }
