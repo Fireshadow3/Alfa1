6b6e892a0521442687626494a02ec62fba895ae2
qwewerertrty <37083556+qwewerertrty@users.noreply.github.com>
2018-03-06 18:56:14 +0100

    Update Main.java

--- Eclipse-project/src/Main.java
+++ Eclipse-project/src/Main.java
@@ -8,18 +8,38 @@ public class Main {
     public static void main(String[] args) throws IOException{
     	CleanFile c = new CleanFile();
     	ArrayList<String> strings;
+    	ArrayList<String> testArrayList;
     	
     	String in = "list.txt";   // file input
     	String out = "monco.txt"; // file output
     	
-    	String m = "abbozzo";
+    	String m = "cin";
     	
-    	c.cleanAndPrintAfterSlash(in, out);
+    	testArrayList = c.deleteWordsAfterSlashCharacter(in, out);
     	
-    	strings = c.anagram(m);
+    	strings = c.anagramGenerator(m);
     	System.out.println(strings);
-    	System.out.println(c.numbersOfAnagrams(out, m));
-    	System.out.println(c.wordsBetweenEightAndEleven(in));
+    	System.out.println(c.numbersOfAnagramsInAnArrayList(testArrayList, m));
+    	System.out.println("Parole comprese tra 8 e 11 caratteri: " + c.wordsBetweenEightAndElevenCharacters(in));
+    	System.out.println();
+    	
+    	int[] testArray = c.frequencyOfAlphabetLetterInDictionary(strings);
+    	char l = 'a';
+    	/*
+    	for (int i : testArray) {
+    		double percentage = c.percentageFrequency((double)i / CleanFile.getManz());
+    		System.out.println(l + ": " + i + ";\t\tFrequenza relativa: " + percentage / 100 + ";\t Frequenza percentuale: " + percentage + "%");
+    		l++;
+    	}
+    	*/
+    	String first = "cni";
+    	String last = "nci";
+    	int[] array = c.frequencyOfAlphabetLetterInDictionaryFromAWordToAnother(strings, first, last);
+    	for (int s : array) {
+    		System.out.println("Lettera: " + l + " -> " + s);
+    		l++;
+    	}
+    	
     }
 
 }
