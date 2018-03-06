30b670d99802a0161be38bdb6983cdee5f98aecb
qwewerertrty <37083556+qwewerertrty@users.noreply.github.com>
2018-03-06 18:55:49 +0100

    Update CleanFile.java

--- Eclipse-project/src/CleanFile.java
+++ Eclipse-project/src/CleanFile.java
@@ -6,77 +6,84 @@ import java.util.*;
 public class CleanFile {
 	static final int MAX_LENGTH = 11;
 	static final int MIN_LENGTH = 8;
+	static final int ALPHABET_LEN = 26;
+	static int TotalLettersInDictionary = 0;
+	static int TotalLettersBetweenTwoWords = 0;
 	
-	public void cleanAndPrintAfterSlash(String input_file, String output_file) throws IOException {
+	public ArrayList<String> deleteWordsAfterSlashCharacter(String input_file, String output_file) throws IOException {
 		// file input
-        File fin = new File(input_file);
-        // file output
-        FileWriter fout = new FileWriter(output_file);
-        // oggetto reader con codifica dei caratteri UTF-16
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(fin), "UTF-16"));
-        // oggetto writer
-        BufferedWriter writer = new BufferedWriter(fout);
-        // stringa per leggere ogni riga del file
-        String line;
-        // ArrayList per rimuovere tutte le parole dopo '/'
-        ArrayList<String> words = new ArrayList<>();
-        // Array finale da stampare su file
-        String[] finalWords;
-        
-        while ((line = reader.readLine()) != null) {
-            if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') ||
-                    (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
-                words.add(line);
-        }
-        // inserisco i caratteri nell'array e stampo
-        for (String element : words) {
-            finalWords = element.split("/");
-            writer.write(finalWords[0] + "\n");
-        }
-        reader.close();
-        writer.close();
+		File fin = new File(input_file);
+		// file output
+		FileWriter fout = new FileWriter(output_file);
+		// oggetto reader con codifica dei caratteri UTF-16
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fin), "UTF-16"));
+		// oggetto writer
+		BufferedWriter writer = new BufferedWriter(fout);
+		// stringa per leggere ogni riga del file
+		String line;
+		// ArrayList per rimuovere tutte le parole dopo '/'
+		ArrayList<String> words = new ArrayList<>();
+		// Array finale da stampare su file
+		String[] finalWords;
+		// output arraylist
+		ArrayList<String> finalArrayList = new ArrayList<>();
+
+		while ((line = reader.readLine()) != null) {
+			if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') || (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
+				words.add(line);
+		}
+		// inserisco i caratteri nell'array e stampo
+		for (String element : words) {
+			finalWords = element.split("/");
+			writer.write(finalWords[0] + "\n");
+			finalArrayList.add(finalWords[0]);
+		}
+		reader.close();
+		writer.close();
+		return finalArrayList;
 	}
 	
-	public int wordsBetweenEightAndEleven(String input_file) throws IOException {
-		// file input
-        File fin = new File(input_file);
-        // oggetto reader con codifica dei caratteri UTF-16
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(fin), "UTF-16"));
-        // stringa per leggere ogni riga del file
-        String line;
-        // ArrayList per rimuovere tutte le parole dopo '/'
-        ArrayList<String> words = new ArrayList<>();
-        // Array finale da stampare su file
-        String[] finalWords;
-        
-        while ((line = reader.readLine()) != null) {
-            if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') ||
-                    (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
-                words.add(line);
-        }
-        int counter = 0;
-        // controllo la lunghezza della parola
-        for (String element : words) {
-            finalWords = element.split("/");
-            if (finalWords[0].length() <= MAX_LENGTH && finalWords[0].length() >= MIN_LENGTH) {
-            	counter++;
-            }
-        }
-        reader.close();
-        return counter;
+	public static int getManz() {
+		return CleanFile.TotalLettersInDictionary;
 	}
 	
+	public int wordsBetweenEightAndElevenCharacters(String input_file) throws IOException {
+		// file input
+		File fin = new File(input_file);
+		// oggetto reader con codifica dei caratteri UTF-16
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fin), "UTF-16"));
+		// stringa per leggere ogni riga del file
+		String line;
+		// ArrayList per rimuovere tutte le parole dopo '/'
+		ArrayList<String> words = new ArrayList<>();
+		// Array finale da stampare su file
+		String[] finalWords;
+
+		while ((line = reader.readLine()) != null) {
+			if ((line.charAt(0) >= 'a' && line.charAt(0) <= 'z') || (line.charAt(0) >= 'A' && line.charAt(0) <= 'Z'))
+				words.add(line);
+		}
+		int counter = 0;
+		// controllo la lunghezza della parola
+		for (String element : words) {
+			finalWords = element.split("/");
+			if (finalWords[0].length() <= MAX_LENGTH && finalWords[0].length() >= MIN_LENGTH) {
+				counter++;
+			}
+		}
+		reader.close();
+		return counter;
+	}
+
 	// genera anagrammi
-	public ArrayList<String> anagram(String s) {
+	public ArrayList<String> anagramGenerator(String s) {
 		ArrayList<String> finalArrayList = new ArrayList<>();
 		if (s.length() <= 1)
 			finalArrayList.add(s);
 		else {
 			for (int i = 0; i < s.length(); i++) {
 				char c = s.charAt(i);
-				ArrayList<String> temp = anagram(s.substring(0, i) + s.substring(i + 1));
+				ArrayList<String> temp = anagramGenerator(s.substring(0, i) + s.substring(i + 1));
 				for (String element : temp) {
 					finalArrayList.add(c + element);
 				}
@@ -84,23 +91,249 @@ public class CleanFile {
 		}
 		return finalArrayList;
 	}
-	
-	// numero anagrammi contenuti nel file
-	public int numbersOfAnagrams(String input_file, String word) throws IOException {
-		ArrayList<String> anagrams = anagram(word);
+
+	// dato un ArrayList, conta il numero di ricorrenze di ogni anagramma
+	public int numbersOfAnagramsInAnArrayList(ArrayList<String> input_ArrayList, String word) {
 		int counter = 0;
-		// file input
-        TextFile file = new TextFile(input_file, 'r');
-		// stringa per leggere ogni riga del file
-        String line;
-		while((line = file.fromFile()) != null) {
-			for (String element : anagrams) {
-				if (line.equalsIgnoreCase(element))
+		// ArrayList anagrammi
+		ArrayList<String> anagrams = anagramGenerator(word);
+		for (String element : input_ArrayList) {
+			for (String s : anagrams) {
+				if (s.equalsIgnoreCase(element))
 					counter++;
 			}
 		}
-		file.close();
 		return counter;
+	}
+
+	// calcola ricorrenza delle lettere dell'alfabeto come percentuale
+	public int[] frequencyOfAlphabetLetterInDictionary(ArrayList<String> input_ArrayList) {
+		// vettore contenente tutte le lettere dell'alfabeto, associate all'indice
+		char[] alphabetArray = new char[ALPHABET_LEN];
+		// array di frequenze assolute
+		int[] alphabetLettersAbsoluteFrequencyArray = new int[ALPHABET_LEN];
+		char letter = 'a';
+		// inizializzo l'array con le lettere dell'alfabeto
+		for (int i = 0; i < ALPHABET_LEN; i++) {
+			alphabetArray[i] = letter;
+			letter++;
+		}
+		for (String element : input_ArrayList) {
+			element = element.toLowerCase();
+			for(int k = 0; k < element.length(); k++, CleanFile.TotalLettersInDictionary++) {
+				switch(element.charAt(k)) {
+				case 'a':
+					alphabetLettersAbsoluteFrequencyArray[0]++;
+					break;
+				case 'b':
+					alphabetLettersAbsoluteFrequencyArray[1]++;
+					break;
+				case 'c':
+					alphabetLettersAbsoluteFrequencyArray[2]++;
+					break;
+				case 'd':
+					alphabetLettersAbsoluteFrequencyArray[3]++;
+					break;
+				case 'e':
+					alphabetLettersAbsoluteFrequencyArray[4]++;
+					break;
+				case 'f':
+					alphabetLettersAbsoluteFrequencyArray[5]++;
+					break;
+				case 'g':
+					alphabetLettersAbsoluteFrequencyArray[6]++;
+					break;
+				case 'h':
+					alphabetLettersAbsoluteFrequencyArray[7]++;
+					break;
+				case 'i':
+					alphabetLettersAbsoluteFrequencyArray[8]++;
+					break;
+				case 'j':
+					alphabetLettersAbsoluteFrequencyArray[9]++;
+					break;
+				case 'k':
+					alphabetLettersAbsoluteFrequencyArray[10]++;
+					break;
+				case 'l':
+					alphabetLettersAbsoluteFrequencyArray[11]++;
+					break;
+				case 'm':
+					alphabetLettersAbsoluteFrequencyArray[12]++;
+					break;
+				case 'n':
+					alphabetLettersAbsoluteFrequencyArray[13]++;
+					break;
+				case 'o':
+					alphabetLettersAbsoluteFrequencyArray[14]++;
+					break;
+				case 'p':
+					alphabetLettersAbsoluteFrequencyArray[15]++;
+					break;
+				case 'q':
+					alphabetLettersAbsoluteFrequencyArray[16]++;
+					break;
+				case 'r':
+					alphabetLettersAbsoluteFrequencyArray[17]++;
+					break;
+				case 's':
+					alphabetLettersAbsoluteFrequencyArray[18]++;
+					break;
+				case 't':
+					alphabetLettersAbsoluteFrequencyArray[19]++;
+					break;
+				case 'u':
+					alphabetLettersAbsoluteFrequencyArray[20]++;
+					break;
+				case 'v':
+					alphabetLettersAbsoluteFrequencyArray[21]++;
+					break;
+				case 'w':
+					alphabetLettersAbsoluteFrequencyArray[22]++;
+					break;
+				case 'x':
+					alphabetLettersAbsoluteFrequencyArray[23]++;
+					break;
+				case 'y':
+					alphabetLettersAbsoluteFrequencyArray[24]++;
+					break;
+				case 'z':
+					alphabetLettersAbsoluteFrequencyArray[25]++;
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		return alphabetLettersAbsoluteFrequencyArray;
+	}	
+	
+	public double percentageFrequency(double i) {
+		return i * 100;
+	}
+	
+	public ArrayList<String> estrazione(ArrayList<String> input_ArrayList, String input_firstWord, String input_lastWord) {
+		ArrayList<String> result = new ArrayList<>();
+		
+		for(String k : input_ArrayList) {
+			if(k.compareTo(input_firstWord) >= 0 && k.compareTo(input_lastWord) <= 0) {
+				result.add(k);
+			}
+		}
+		return result;
+	}
+	
+	
+	// calcola ricorrenza delle lettere dell'alfabeto come percentuale
+	// da parola a parola
+	public int[] frequencyOfAlphabetLetterInDictionaryFromAWordToAnother(ArrayList<String> input_ArrayList, String input_firstWord, String input_lastWord) {
+		// vettore contenente tutte le lettere dell'alfabeto, associate all'indice
+		char[] alphabetArray = new char[ALPHABET_LEN];
+		// array di frequenze assolute
+		int[] alphabetLettersAbsoluteFrequencyArray = new int[ALPHABET_LEN];
+		ArrayList<String> brutto;
+		char letter = 'a';
+		// inizializzo l'array con le lettere dell'alfabeto
+		for (int i = 0; i < ALPHABET_LEN; i++) {
+			alphabetArray[i] = letter;
+			letter++;
+		}
+		// controllo se le parole sono contenute nel dizionario
+		if (!(input_ArrayList.contains(input_firstWord) && input_ArrayList.contains(input_lastWord))) {
+			System.out.println("The words are not in the dictionary");
+			return alphabetLettersAbsoluteFrequencyArray;
+		}
+		
+		brutto = estrazione(input_ArrayList, input_firstWord, input_lastWord);
+		
+		for (String element : brutto) {
+			element = element.toLowerCase();
+				for(int k = 0; k < element.length(); k++, CleanFile.TotalLettersInDictionary++) {
+					switch(element.charAt(k)) {
+					case 'a':
+						alphabetLettersAbsoluteFrequencyArray[0]++;
+						break;
+					case 'b':
+						alphabetLettersAbsoluteFrequencyArray[1]++;
+						break;
+					case 'c':
+						alphabetLettersAbsoluteFrequencyArray[2]++;
+						break;
+					case 'd':
+						alphabetLettersAbsoluteFrequencyArray[3]++;
+						break;
+					case 'e':
+						alphabetLettersAbsoluteFrequencyArray[4]++;
+						break;
+					case 'f':
+						alphabetLettersAbsoluteFrequencyArray[5]++;
+						break;
+					case 'g':
+						alphabetLettersAbsoluteFrequencyArray[6]++;
+						break;
+					case 'h':
+						alphabetLettersAbsoluteFrequencyArray[7]++;
+						break;
+					case 'i':
+						alphabetLettersAbsoluteFrequencyArray[8]++;
+						break;
+					case 'j':
+						alphabetLettersAbsoluteFrequencyArray[9]++;
+						break;
+					case 'k':
+						alphabetLettersAbsoluteFrequencyArray[10]++;
+						break;
+					case 'l':
+						alphabetLettersAbsoluteFrequencyArray[11]++;
+						break;
+					case 'm':
+						alphabetLettersAbsoluteFrequencyArray[12]++;
+						break;
+					case 'n':
+						alphabetLettersAbsoluteFrequencyArray[13]++;
+						break;
+					case 'o':
+						alphabetLettersAbsoluteFrequencyArray[14]++;
+						break;
+					case 'p':
+						alphabetLettersAbsoluteFrequencyArray[15]++;
+						break;
+					case 'q':
+						alphabetLettersAbsoluteFrequencyArray[16]++;
+						break;
+					case 'r':
+						alphabetLettersAbsoluteFrequencyArray[17]++;
+						break;
+					case 's':
+						alphabetLettersAbsoluteFrequencyArray[18]++;
+						break;
+					case 't':
+						alphabetLettersAbsoluteFrequencyArray[19]++;
+						break;
+					case 'u':
+						alphabetLettersAbsoluteFrequencyArray[20]++;
+						break;
+					case 'v':
+						alphabetLettersAbsoluteFrequencyArray[21]++;
+						break;
+					case 'w':
+						alphabetLettersAbsoluteFrequencyArray[22]++;
+						break;
+					case 'x':
+						alphabetLettersAbsoluteFrequencyArray[23]++;
+						break;
+					case 'y':
+						alphabetLettersAbsoluteFrequencyArray[24]++;
+						break;
+					case 'z':
+						alphabetLettersAbsoluteFrequencyArray[25]++;
+						break;
+					default:
+						break;
+					}	
+			}
+		}
+		return alphabetLettersAbsoluteFrequencyArray;
 	}
 	
 }
