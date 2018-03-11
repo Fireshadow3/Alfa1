7df97c89e195060cef208fd82a1adf70372e94e4
qwewerertrty <37083556+qwewerertrty@users.noreply.github.com>
2018-03-11 21:23:58 +0100

    Add files via upload

--- First/test.java
+++ First/test.java
@@ -4,13 +4,14 @@
  * and open the template in the editor.
  */
 package boccaccio;
+import java.io.IOException;
 import java.util.*;
 /**
  *
  * @author Fabio
  */
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AnagramManagement anagrammos = new AnagramManagement();
         WordsInDictionaryManagement word = new WordsInDictionaryManagement();
         FileManagement file = new FileManagement();
@@ -18,22 +19,10 @@ public class test {
         ArrayList<String> stringsOnFile;
         HashMap<Character, Integer> af;
         
-        ArrayList<String> pippo = new ArrayList<>(Arrays.asList("ab", "B", "Cin", "D","inc", "nci"));
+        ArrayList<String> pippo = new ArrayList<>(Arrays.asList("AIDS", "Abbado", "ab","està", "nci"));
         System.out.println(anagrammos.AnagramsInAnArrayList(pippo, "ab"));
         
-        af = word.frequencyOfAlphabetLetterInDictionary(pippo);
-        System.out.println(af);
-        
-        af = word.frequencyOfAlphabetLetterInDictionaryFromAWordToAnother(pippo, "cin", "inc");
-        System.out.println(af);
-        
-        substringsAnagrams = anagrammos.SubstringsAnagrams("cane");
-        System.out.println(substringsAnagrams);
-        
-        substringsAnagrams = anagrammos.SubstringAnagramsForWordLenghtGreaterThanThree("ab", pippo);
-        System.out.println(substringsAnagrams);
-        
-        stringsOnFile = file.WriteOnFileWordsBetweenThreeAndSevenLetters("boccaccio.txt", pippo);
+        stringsOnFile = file.WriteOnFileWordsBetweenThreeAndSevenLetters("Pippo.txt", pippo);
         System.out.println(stringsOnFile);
     }
 }
