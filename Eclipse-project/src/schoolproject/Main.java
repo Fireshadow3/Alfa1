3771be520e8b8aebdfccf4255e9d985c63195d0d
qwewerertrty <37083556+qwewerertrty@users.noreply.github.com>
2018-03-14 21:41:38 +0100

    Update Main.java

--- Eclipse-project/src/schoolproject/Main.java
+++ Eclipse-project/src/schoolproject/Main.java
@@ -14,20 +14,26 @@ public class Main {
         man.deleteWordsAfterSlashCharacter();
         System.out.println("Words between eight and eleven characters: " + man.wordsBetweenEightAndElevenCharacters());
         System.out.println();
-        System.out.println("Letters between three and seven characters: " + man.wordsBetweenThreeAndSevenLetters().toString());
+        System.out.println("Letters between three and seven characters: "
+                + man.wordsBetweenThreeAndSevenLetters().toString());
         man.writeOnFileWordsBetweenThreeAndSevenLetters("3to7.txt");
         System.out.println();
         System.out.println("All \"afta\" anagrams: " + man.anagramGenerator(anag).toString());
         System.out.println();
-        System.out.println("All \"afta\" anagrams contained in the array: " + man.anagramsInAnArrayList(anag).toString());
+        System.out.println("All \"afta\" anagrams contained in the array: "
+                + man.anagramsInAnArrayList(anag).toString());
         System.out.println();
         System.out.println("Substrings of \"afta\": " + man.substringsAnagrams(anag));
         System.out.println();
-        System.out.println("Substrings of \"afta\" (it's greater than three): " + man.substringAnagramsForWordLengthGreaterThanThree(anag));
+        System.out.println("Substrings of \"afta\" (it's greater than three): "
+                + man.substringAnagramsForWordLengthGreaterThanThree(anag));
         System.out.println();
-        System.out.println("Frequency of letters in dictionary: " + man.frequencyOfAlphabetLetterInDictionary().toString());
+        System.out.println("Frequency of letters in dictionary: "
+                + man.frequencyOfAlphabetLettersInDictionary().toString());
         System.out.println();
-        System.out.println("Frequency of letters in dictionary from a word to another: " + man.frequencyOfAlphabetLetterInDictionaryFromAWordToAnother("abasia", "abbadingo"));
+        System.out.println("Frequency of letters in dictionary from a word to another: "
+                + man.frequencyOfAlphabetLetterInDictionaryFromAWordToAnother
+                ("abasia", "abbadingo"));
         System.out.println();
     }
 }
