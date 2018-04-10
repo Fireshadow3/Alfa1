03daa3b87a22f32c7723303450e15a888ef65c16
Fireshadow3 <alexboss10001@gmail.com>
2018-04-10 16:57:46 +0200

    Fixes to project

--- Eclipse-project/src/schoolproject/FileManagement.java
+++
@@ -1,9 +0,0 @@
-package schoolproject;
-
-import java.util.ArrayList;
-
-public interface FileManagement {
-    ArrayList<String> deleteWordsAfterSlashCharacter();
-
-    ArrayList<String> writeOnFileWordsBetweenThreeAndSevenLetters(String output_file);
-}
